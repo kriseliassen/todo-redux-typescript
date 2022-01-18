fb3895abd96be9c15635b4325fa103cb6acfc414
Kristin Eliassen <kristin.eliassen@gmail.com>
2022-01-18 14:28:44 +0100

    Implemented update todo function

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { removeTodo, updateTodo } from "../redux/todoSlice";
+import { useForm, SubmitHandler } from 'react-hook-form'
 
 interface IProps {
   title: string,
@@ -8,27 +9,72 @@ interface IProps {
   completed: boolean
   id: string
 }
+
+interface Inputs {
+  title: string,
+  description?: string,
+}
+
 const TodoItem: React.FC<IProps> = ({ title, description, id, completed }) => {
   const [currentTodo, setCurrentTodo] = useState<IProps>({ title, description, id, completed })
+  const [isEditing, setIsEditing] = useState<boolean>(false);
 
   const dispatch = useDispatch()
 
-  const deleteTodo = () => {
+  const { register, handleSubmit } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = data => {
+    dispatch(updateTodo(currentTodo));
+    setIsEditing(false);
+  };
+
+  const deleteTodo = (): void => {
     dispatch(removeTodo(id))
   }
-  const editTodo = () => {
-    console.log(currentTodo);
+
+  const editTodo = (e: React.SyntheticEvent): void => {
+    e.stopPropagation();
+    setIsEditing(true);
+    setCurrentTodo({ title, description, id, completed })
+    console.log('CLICKED TODO', currentTodo);
     // dispatch(updateTodo(id))
   }
 
   return (
     <div>
-      <div>
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-      <button onClick={editTodo}>Remove</button>
-      <button onClick={deleteTodo}>Remove</button>
+      {isEditing ? (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label>Title</label>
+          <input
+            type="text"
+            defaultValue={currentTodo.title}
+            {...register('title')}
+            onChange={(e) => setCurrentTodo({ ...currentTodo, title: e.target.value })}
+          />
+          <label>Description</label>
+          <input
+            type="text"
+            defaultValue={currentTodo.description}
+            {...register('description')}
+            onChange={(e) => setCurrentTodo({ ...currentTodo, description: e.target.value })}
+          />
+          <input type="submit" value="Update" />
+          <input
+            type="button"
+            value="Cancel"
+            onClick={() => setIsEditing(false)} />
+        </form>
+      )
+        : (
+          <>
+            <div>
+              <h3>{title}</h3>
+              <p>{description}</p>
+            </div>
+            <button onClick={editTodo}>Edit</button>
+            <button onClick={deleteTodo}>Remove</button>
+          </>
+        )
+      }
     </div>
   )
 }
